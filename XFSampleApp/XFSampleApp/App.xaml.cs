301b89ed200e8c402953e948f96a4070714bfbf4
James Tsai <jatsai@msn.com>
2020-01-16 16:12:41 +0800

    First Commit.

---
+++ XFSampleApp/XFSampleApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFSampleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Device.SetFlags(new[] {
+                                "CarouselView_Experimental",
+                                "IndicatorView_Experimental",
+                                "FastRenderers_Experimental"
+                            });
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XFSampleApp/XFSampleApp/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFSampleApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
